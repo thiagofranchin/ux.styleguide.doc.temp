ecd1e87e174fbde6876a3befc7210fb0ddc806c5
Thiago Franchin <thiago.franchin@i-value.int>
2017-05-29 17:57:21 -0300

    style sublink

--- src/components/menu-lateral.js
+++ src/components/menu-lateral.js
@@ -9,12 +9,12 @@ export default class MenuLateral extends Component {
 
   render(){
     return(
-      <div id="accordion" role="tablist" aria-multiselectable="true">
+      <div id="accordion" className="menu-lateral" role="tablist" aria-multiselectable="true">
         <MenuLateralLink
-          txtLinkMenu="Colors"
+          txtLinkMenu="Spinner"
           idMenu="a" 
-          indexRef="aa" 
-        >          
+          indexRef="aa" >
+
           <MenuLateralSubMenuLinks
             txtLinkSubMenu="Classe de Cores" 
             linkSubMenu="http://www.gmail.com.br"
@@ -23,18 +23,102 @@ export default class MenuLateral extends Component {
             txtLinkSubMenu="Cores das Marcas" 
             linkSubMenu="http://www.gmail.com.br"
           />
-
         </MenuLateralLink>
 
         <MenuLateralLink
-          txtLinkMenu="Borders"         
+          txtLinkMenu="Cores"         
           idMenu="b"
-          indexRef="bb"
-        >
+          indexRef="bb" >
+
           <MenuLateralSubMenuLinks
             txtLinkSubMenu="Sub b"
             linkSubMenu="http://www.google.com.br"
           />
+        </MenuLateralLink>
+        
+        <MenuLateralLink
+          txtLinkMenu="Icons"         
+          idMenu="c"
+          indexRef="cc" >
+
+          <MenuLateralSubMenuLinks
+            txtLinkSubMenu="Sub b"
+            linkSubMenu="http://www.google.com.br"
+          />          
+        </MenuLateralLink>
+
+        <MenuLateralLink
+          txtLinkMenu="Select Control"         
+          idMenu="d"
+          indexRef="dd" >
+
+          <MenuLateralSubMenuLinks
+            txtLinkSubMenu="Checkbox Switch"
+            linkSubMenu="http://www.google.com.br"
+          />          
+          <MenuLateralSubMenuLinks
+            txtLinkSubMenu="Checkbox Custom"
+            linkSubMenu="http://www.google.com.br"
+          />
+          <MenuLateralSubMenuLinks
+            txtLinkSubMenu="Radio button"
+            linkSubMenu="http://www.google.com.br"
+          />
+        </MenuLateralLink>
+        
+        <MenuLateralLink
+          txtLinkMenu="Buttons"         
+          idMenu="e"
+          indexRef="ee" >
+
+          <MenuLateralSubMenuLinks
+            txtLinkSubMenu="Sub b"
+            linkSubMenu="http://www.google.com.br"
+          />          
+        </MenuLateralLink>
+
+        <MenuLateralLink
+          txtLinkMenu="Logos"         
+          idMenu="f"
+          indexRef="ff" >
+
+          <MenuLateralSubMenuLinks
+            txtLinkSubMenu="Sub b"
+            linkSubMenu="http://www.google.com.br"
+          />          
+        </MenuLateralLink>
+
+        <MenuLateralLink
+          txtLinkMenu="Illustration"         
+          idMenu="g"
+          indexRef="gg" >
+
+          <MenuLateralSubMenuLinks
+            txtLinkSubMenu="Sub b"
+            linkSubMenu="http://www.google.com.br"
+          />          
+        </MenuLateralLink>
+
+        <MenuLateralLink
+          txtLinkMenu="Popover"         
+          idMenu="h"
+          indexRef="hh" >
+
+          <MenuLateralSubMenuLinks
+            txtLinkSubMenu="Sub b"
+            linkSubMenu="http://www.google.com.br"
+          />          
+        </MenuLateralLink>
+
+        <MenuLateralLink
+          txtLinkMenu="Borders"         
+          idMenu="i"
+          indexRef="ii" >
+
+          <MenuLateralSubMenuLinks
+            txtLinkSubMenu="Sub b"
+            linkSubMenu="http://www.google.com.br"
+          />          
         </MenuLateralLink>
       </div>
     )
